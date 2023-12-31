e27a624794ee76a84a3969eacf8aeb8cfb7164a4
Carnotita <86444149+Carnotita@users.noreply.github.com>
2023-12-31 09:30:31 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "psipatricia.github.io",
+  base:'/' "psipatricia.github.io",
   plugins: [react()],
 })
